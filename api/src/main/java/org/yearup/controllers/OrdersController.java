db2978e1180a2b7f15b03e1595b5d02b65106d37
Estifanos Teklearegay <estifoteklearegay@gmail.com>
2025-12-17 15:27:07 -0800

    created order model

---
+++ api/src/main/java/org/yearup/controllers/OrdersController.java
@@ -0,0 +1,12 @@
+package org.yearup.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class OrdersController {
+
+}
